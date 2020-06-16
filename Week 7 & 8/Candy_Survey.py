8f5df9d16d2fbd6abd5e71d802ff1aa5d8f55d3b
Gabriel Valenzuela <54542151+gabriel-valenzuela@users.noreply.github.com>
2020-06-16 15:36:46 -0500

    Add files via upload

---
+++ Week 7 & 8/Candy_Survey.py
@@ -0,0 +1,119 @@
+	'Internal ID'	
+	'Q1: GOING OUT?'	
+	'Q2: GENDER	'
+	'Q3: AGE	'
+	'Q4: COUNTRY'	
+	'Q5: STATE, PROVINCE, COUNTY, ETC'	
+	'Q6 | 100 Grand Bar	'
+	'Q6 | Anonymous brown globs that come in black and orange wrappers	(a.k.a. Mary Janes)'	
+	'Q6 | Any full-sized candy bar	'
+	'Q6 | Black Jacks	'
+	'Q6 | Bonkers (the candy)	'
+	'Q6 | Bonkers (the board game)	'
+	'Q6 | Bottle Caps	'
+	'Q6 | Box'o'Raisins	'
+	'Q6 | Broken glow stick'	
+	'Q6 | Butterfinger	'
+	'Q6 | Cadbury Creme Eggs	
+	'Q6 | Candy Corn	
+	'Q6 | Candy that is clearly just the stuff given out for free at restaurants	
+	'Q6 | Caramellos	
+	'Q6 | Cash, or other forms of legal tender	
+	'Q6 | Chardonnay	
+	'Q6 | Chick-o-Sticks (we donÕt know what that is)	
+	'Q6 | Chiclets	
+	'Q6 | Coffee Crisp	
+	'Q6 | Creepy Religious comics/Chick Tracts	
+	'Q6 | Dental paraphenalia	
+	'Q6 | Dots	
+	'Q6 | Dove Bars	
+	'Q6 | Fuzzy Peaches	
+	'Q6 | Generic Brand Acetaminophen	
+	'Q6 | Glow sticks	
+	'Q6 | Goo Goo Clusters	
+	'Q6 | Good N' Plenty	
+	'Q6 | Gum from baseball cards	
+	'Q6 | Gummy Bears straight up	
+	'Q6 | Hard Candy	
+	'Q6 | Healthy Fruit	
+	'Q6 | Heath Bar	
+	'Q6 | Hershey's Dark Chocolate	
+	'Q6 | HersheyÕs Milk Chocolate	
+	'Q6 | Hershey's Kisses	
+	'Q6 | Hugs (actual physical hugs)	
+	'Q6 | Jolly Rancher (bad flavor)	
+	'Q6 | Jolly Ranchers (good flavor)	
+	'Q6 | JoyJoy (Mit Iodine!)	
+	'Q6 | Junior Mints	
+	'Q6 | Senior Mints	
+	'Q6 | Kale smoothie	
+	'Q6 | Kinder Happy Hippo	
+	'Q6 | Kit Kat	
+	'Q6 | LaffyTaffy	
+	'Q6 | LemonHeads	
+	'Q6 | Licorice (not black)	
+	'Q6 | Licorice (yes black)	
+	'Q6 | Lindt Truffle	
+	'Q6 | Lollipops	
+	'Q6 | Mars	
+	'Q6 | Maynards	
+	'Q6 | Mike and Ike	
+	'Q6 | Milk Duds	
+	'Q6 | Milky Way	
+	'Q6 | Regular M&Ms	
+	'Q6 | Peanut M&MÕs	
+	'Q6 | Blue M&M's	
+	'Q6 | Red M&M's	
+	'Q6 | Green Party M&M's	
+	'Q6 | Independent M&M's	
+	'Q6 | Abstained from M&M'ing.	
+	'Q6 | Minibags of chips	
+	'Q6 | Mint Kisses	
+	'Q6 | Mint Juleps	
+	'Q6 | Mr. Goodbar	
+	'Q6 | Necco Wafers	
+	'Q6 | Nerds	
+	'Q6 | Nestle Crunch	
+	'Q6 | Now'n'Laters	
+	'Q6 | Peeps	
+	'Q6 | Pencils	
+	'Q6 | Pixy Stix	
+	'Q6 | Real Housewives of Orange County Season 9 Blue-Ray	
+	'Q6 | ReeseÕs Peanut Butter Cups	'
+	'Q6 | Reese's Pieces	'
+	'Q6 | Reggie Jackson Bar	'
+	'Q6 | Rolos	'
+	'Q6 | Sandwich-sized bags filled with BooBerry Crunch	'
+	'Q6 | Skittles	'
+	'Q6 | Smarties (American)	'
+	'Q6 | Smarties (Commonwealth)	'
+	'Q6 | Snickers	'
+	'Q6 | Sourpatch Kids (i.e. abominations of nature)	'
+	'Q6 | Spotted Dick	'
+	'Q6 | Starburst	'
+	'Q6 | Sweet Tarts	'
+	'Q6 | Swedish Fish	'
+	'Q6 | Sweetums (a friend to diabetes)	'
+	'Q6 | Take 5	'
+	'Q6 | Tic Tacs	'
+	'Q6 | Those odd marshmallow circus peanut things	'
+	'Q6 | Three Musketeers	'
+	'Q6 | Tolberone something or other	'
+	'Q6 | Trail Mix	'
+	'Q6 | Twix	'
+	'Q6 | Vials of pure high fructose corn syrup, for main-lining into your vein'	
+	'Q6 | Vicodin	'
+	'Q6 | Whatchamacallit Bars	'
+	'Q6 | White Bread	'
+	'Q6 | Whole Wheat anything	'
+	'Q6 | York Peppermint Patties'	
+	'Q7: JOY OTHER	'
+	'Q8: DESPAIR OTHER	'
+	'Q9: OTHER COMMENTS'	
+	'Q10: DRESS	'	
+	'Q11: DAY	'
+	'Q12: MEDIA [Daily Dish]'	
+	'Q12: MEDIA [Science]'	
+	'Q12: MEDIA [ESPN]	'
+	'Q12: MEDIA [Yahoo]	'
+	'Click Coordinates (x, y)'
